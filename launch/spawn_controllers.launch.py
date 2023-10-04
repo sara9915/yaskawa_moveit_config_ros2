186ff8f2bb37d518b185ed4888d1868673348692
Sara Federico <sara.federico99@hotmail.it>
2023-10-04 09:55:00 +0200

    Fixed name

--- launch/spawn_controllers.launch.py
+++ launch/spawn_controllers.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_spawn_controllers_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("motoman_sia5f", package_name="yaskawa_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("motoman_sia5f", package_name="yaskawa_moveit_config_ros2").to_moveit_configs()
     return generate_spawn_controllers_launch(moveit_config)
