6409bca9750cfddc184ee13473ee754fd334be3d
Sara Federico <sara.federico99@hotmail.it>
2023-10-26 12:13:28 +0200

    Add ompl_planning.yaml and chomp.yaml

--- launch/demo.launch.py
+++ launch/demo.launch.py
@@ -3,5 +3,6 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("motoman_sia5f", package_name="yaskawa_moveit_config_ros2").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("motoman_sia5f", package_name="yaskawa_moveit_config_ros2").planning_pipelines(
+            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]).to_moveit_configs()
     return generate_demo_launch(moveit_config)
